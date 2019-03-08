0a4ebf58b66f15f8aac07e49a037e86cce496fef
qyzxcswbll <136552740@qq.com>
2019-03-08 11:47:28 +0800

    *****************

---
+++ line/demo/grpc/Multiple/client/run/run.go
@@ -0,0 +1,14 @@
+/*
+@Time :  2019/3/7 17:43
+@Author : niko
+@File : run
+@Software: GoLand
+*/
+package main
+
+import "github.com/scryinfo/dot/line/demo/grpc/Multiple/client"
+
+func main()  {
+	client.A()
+	//client.B()
+}
