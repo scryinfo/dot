b41cc160002cf252f90445d343501b16fa62d62c
Peacess <peacess@163.com>
2019-04-23 18:42:28 +0800

    set private field

--- demo/inject/main.go
+++ demo/inject/main.go
@@ -3,22 +3,65 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"unsafe"
 )
 
 func main()  {
-	t := &T{f:10}
+	SetPrivatef()
+}
 
-	v := reflect.ValueOf(t).Elem()
 
-	f := v.FieldByName("f")
-	fmt.Println(f.CanSet())
+func SetPrivatef()  {
+	t := &T{fPtr: nil,fInterface:nil}
+	f := reflect.ValueOf(t).Elem().FieldByName("fPtr")
+	fmt.Println(f.CanAddr()) // have to true
+	if f.CanAddr() {
+		fp := f.Addr().Pointer()
+		fpp := ((**int)(unsafe.Pointer(fp)))
 
-	f.Set(reflect.ValueOf(10))
+		t2 := &T2{f2: 10}
+		fp2 := (*int)(unsafe.Pointer(t2))
 
-	fmt.Println(t.f)
+		*fpp = fp2
+		fmt.Println(t.fPtr.f2)
+	}
+
+	f = reflect.ValueOf(t).Elem().FieldByName("fInterface")
+	if f.CanAddr() {
+		fp := f.Addr().Pointer()
+		fpp := ((*interface{})(unsafe.Pointer(fp)))
+
+		//var t2 Int2 = &Int2Imp{F: 20}
+		//fp2 := (*interface{})(unsafe.Pointer(&t2))
+		//*fpp = *fp2
+
+		var t2 interface{} = &Int2Imp{F: 20}
+		fp2 := (*interface{})(unsafe.Pointer(reflect.ValueOf(t2).Pointer()))
+
+		*fpp = fp2
+		fmt.Println(t.fInterface.Get())
+	}
 
 }
 
+
 type T struct {
-	f int
+	fPtr       *T2
+	fInterface Int2
+}
+
+type T2 struct {
+	f2 int
+}
+
+type Int2 interface {
+	Get() int
+}
+
+type Int2Imp struct {
+	F int
+}
+
+func (c *Int2Imp) Get() int {
+	return c.F
 }
