5cce9b428f73b2c187c04f8821a29a1df6267d9e
jarvan@scry.info <963315635@qq.com>
2019-08-05 11:32:46 +0800

    home.vue commit

--- tools/config/ui/apps/src/App.vue
+++ tools/config/ui/apps/src/App.vue
@@ -1,20 +1,17 @@
 <template>
-  <router-view></router-view>
+  <div id="app">
+    <Home></Home>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import Home from './views/Home'
+  export default {
+    components:{
+      Home,
+    },
+    name: 'App'
+  }
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- tools/config/ui/apps/src/views/Home.vue
+++ tools/config/ui/apps/src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <el-container>
+  <el-header>Dot Config</el-header>
+  <el-container>
+  <el-aside width="200px">
+     <el-menu >
+       <router-link to="/findDot" style="text-decoration: none;">
+          <el-menu-item index="1">
+            <template slot="title"><i class="el-icon-search"></i>Find Dot</template>
+          </el-menu-item>
+       </router-link>
+       <router-link to="/dotList" style="text-decoration: none;">
+          <el-menu-item index="2">
+            <template slot="title"><i class="el-icon-tickets"></i>Dot List</template>
+          </el-menu-item>
+       </router-link>
+       <router-link to="/config" style="text-decoration: none;">
+          <el-menu-item index="3">
+            <template slot="title"><i class="el-icon-edit"></i>Config</template>
+          </el-menu-item>
+       </router-link>
+       <router-link to="/import" style="text-decoration: none;">
+          <el-menu-item index="4">
+            <template slot="title"><i class="el-icon-upload2"></i>Import</template>
+          </el-menu-item>
+       </router-link>
+       <router-link to="/export" style="text-decoration: none;">
+          <el-menu-item index="5">
+            <template slot="title"><i class="el-icon-download"></i>Export</template>
+          </el-menu-item>
+       </router-link>
+        </el-menu>
+  </el-aside>
+  <el-main>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </el-main>
+  </el-container>
+  </el-container>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script>
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
 </script>
+
+<style>
+    .el-header {
+      background-color: #B3C0D1;
+      color: #333;
+      text-align: center;
+      line-height: 60px;
+    }
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    /*text-align: right;*/
+    /*line-height: 50px;*/
+  }
+.el-menu-item {
+  background: #D3DCE6;
+}
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+    /*a {*/
+    /*  text-decoraction: none;*/
+    /*}*/
+    /*.router-link-active {*/
+    /*  text-decoration: none;*/
+    /*}*/
+</style>
