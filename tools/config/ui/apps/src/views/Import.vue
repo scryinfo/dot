233b3472633f55c737047a5eecf68a88c052afea
babybabyboy <15884273030@163.com>
2019-08-15 18:08:26 +0800

    fix load by config bug and complete import config

--- tools/config/ui/apps/src/views/Import.vue
+++ tools/config/ui/apps/src/views/Import.vue
@@ -1,60 +1,139 @@
 <template>
-  <div style="line-height: 50px">
-    <el-container id="importDot" style="margin-top: -12px;">
-      <el-header id="dotH" style="line-height: 30px;height: 30px;text-align: left;background-color: #D3DCE6;">Dot</el-header>
-      <el-main id="dotM">
-        <div >
-          <span>dotPath :</span>
-          <el-input type="text"  v-model="dotPath" style="width: 60%;margin-left: 2%;"></el-input>
-          <el-button id="removeD" @click="delDotPath()" style="margin-left: 2%">remove</el-button>
-        </div>
-        <el-row>
-          <el-button id="findD" @click="importDot()" style="margin-right:30%;margin-left: 10%;">Import Dot</el-button>
+    <div style="line-height: 50px">
+        <el-container id="importDot" style="margin-top: -12px;">
+            <el-header id="dotH" style="line-height: 30px;height: 30px;text-align: left;background-color: #D3DCE6;">
+                Dot
+            </el-header>
+            <el-main id="dotM">
+                <div>
+                    <span>dotPath :</span>
+                    <el-input type="text" v-model="dotPath" style="width: 60%;margin-left: 2%;"></el-input>
+                    <el-button id="removeD" @click="delDotPath()" style="margin-left: 2%">remove</el-button>
+                </div>
+                <el-row>
+                    <el-button id="findD" @click="importDot()" style="margin-right:30%;margin-left: 10%;">Import Dot
+                    </el-button>
 
-        </el-row>
-      </el-main>
-    </el-container>
-    <el-container id="importConf">
-      <el-header id="confH" style="height: 30px;line-height: 30px; text-align: left;background-color: #D3DCE6;">Config</el-header>
-      <el-main id="confM">
-        <div >
-          <span>confPath:</span>
-          <el-input type="text"  v-model="confPath" style="width: 60%;margin-left: 2%;"></el-input>
-          <el-button id="removeC" @click="delConfPath()" style="margin-left: 2%">remove</el-button>
-          <!--          <el-button id="removeC" @click="del(index)" style="margin-left: 2%">remove</el-button>-->
+                </el-row>
+            </el-main>
+        </el-container>
+        <el-container id="importConf">
+            <el-header id="confH" style="height: 30px;line-height: 30px; text-align: left;background-color: #D3DCE6;">
+                Config
+            </el-header>
+            <el-main id="confM">
+                <div>
+                    <span>confPath:</span>
+                    <el-input type="text" v-model="confPath" style="width: 60%;margin-left: 2%;"></el-input>
+                    <el-button id="removeC" @click="delConfPath()" style="margin-left: 2%">remove</el-button>
+                    <!--          <el-button id="removeC" @click="del(index)" style="margin-left: 2%">remove</el-button>-->
 
-        </div>
-        <el-row>
-          <el-button id="findC" @click="importConf()" style="margin-right:29%;margin-left: 10%;">ImportConfig</el-button>
-        </el-row>
-      </el-main>
-    </el-container>
-  </div>
+                </div>
+                <el-row>
+                    <el-button id="findC" @click="importConf()" style="margin-right:29%;margin-left: 10%;">
+                        ImportConfig
+                    </el-button>
+                </el-row>
+            </el-main>
+        </el-container>
+    </div>
 
 
 </template>
 
 <script>
 
-  export default {
-    data() {
-      return {
-        dotPath:'',
-        confPath:'',
+    import JsonRelyButton from "../components/JsonRelyButton";
 
-      }
-    },
-    methods: {
-      importDot(){},
-      importConf(){},
-      delDotPath(){
-        this.dotPath='';
-      },
-      delConfPath(){
-        this.confPath='';
-      },
+    export default {
+        data() {
+            return {
+                dotPath: '',
+                confPath: '',
+
+            }
+        },
+        methods: {
+            importDot() {
+            },
+            importConf() {
+                if (this.confPath != '') {
+                    let {rpcimportByConfig} = require('../plugins/rpcInterface');
+                    rpcimportByConfig(this.confPath, (response) => {
+                        if (response.getError() != '') {
+                            let err = response.getError()
+                            console.log(err)
+                            this.$message({
+                                message: err,
+                                type: 'error'
+                            })
+                        } else {
+                            let config;
+                            {
+                                let ob = JSON.parse(response.getJson());
+                                config = ob.dots;
+                            }
+                            for (let i = 0, len = config.length; i < len; i++) {
+                                let dot = this.findDot(config[i].metaData.typeId);
+                                this.assembleByTypeId(dot, config[i]);
+                                this.$message({
+                                    message: 'Import success!',
+                                    type: 'success'
+                                });
+                            }
+                        }
+                    });
+                } else {
+                    this.$message({
+                        message: 'Please input confPath!',
+                        type: 'error'
+                    })
+                }
+
+
+            },
+            delDotPath() {
+                this.dotPath = '';
+            },
+            delConfPath() {
+                this.confPath = '';
+            },
+            findDot(typeId) {
+                for (let i = 0, len = this.$root.DotsTem.length; i < len; i++) {
+                    if (this.$root.DotsTem[i].metaData.typeId === typeId) {
+                        return this.$root.Dots[i];
+                    }
+                }
+                return null;
+            },
+            assemble(schema, source) {
+                for (let key in schema) {
+                    schema[key] = this.isObject(schema[key]) ? this.assemble(schema[key], (source[key] ? source[key] : schema[key])) : (source[key] ? source[key] : schema[key]);
+                }
+                return schema;
+            },
+            isObject(o) {
+                return (typeof o === 'object') && o !== null;
+            },
+            assembleByTypeId(dot, config) {
+                let flag = false;
+                for (let i = 0, len = this.$root.Configs.length; i < len; i++) {
+                    if (this.$root.Configs[i].metaData.typeId === config.metaData.typeId) {
+                        this.$root.Configs[i] = this.assemble(this.$root.Configs[i], config);
+                        flag = true;
+                    }
+                }
+                if (!flag) {
+                    if (dot) {
+                        this.$root.Configs.push(this.assemble(dot, config));
+                    } else {
+                        config.metaData.flag = 'not-exist';
+                        this.$root.Configs.push(config);
+                    }
+                }
+            }
+        }
     }
-  }
 </script>
 <style>
 
