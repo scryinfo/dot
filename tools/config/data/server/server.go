0fe5b968ec7a4a7eb14b4fea8c27836f706977ba
Peng-Kun-Dai <15202844993@163.com>
2019-08-20 17:34:31 +0800

    debug

--- tools/config/data/server/server.go
+++ tools/config/data/server/server.go
@@ -4,34 +4,82 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/scryinfo/dot/dot"
 	"github.com/scryinfo/dot/dots/grpc/gserver"
 	"github.com/scryinfo/dot/dots/line"
 	"github.com/scryinfo/dot/tools/config/data/nobl"
 	"github.com/scryinfo/scryg/sutils/ssignal"
 	"go.uber.org/zap"
+	"log"
+	"net/http"
 	"os"
+	"os/exec"
+	"runtime"
 )
 
 func main() {
-	l, err := line.BuildAndStart(add) //first step create line and dots
-	if err != nil {
-		dot.Logger().Errorln("", zap.Error(err))
-		return
+	{
+		ch := make(chan byte, 2)
+		go func() {
+			gin.SetMode(gin.ReleaseMode)
+			router := gin.Default()
+			router.StaticFS("/", http.Dir("../../ui/apps/dist"))
+			ch <- 1
+			router.Run(":9090")
+
+		}()
+
+		switch runtime.GOOS {
+		case "windows":
+			windowsBrowser(ch)
+		case "linux":
+			linuxBrowser(ch)
+		default:
+			log.Fatal("无法识别的操作系统")
+		}
+		<-ch
+		<-ch
 	}
-	defer line.StopAndDestroy(l, true) //fourth step stop and destroy dots
 
-	dot.Logger().Infoln("dot ok")
-	//second step ....
+	{
+		l, err := line.BuildAndStart(add) //first step create line and dots
+		if err != nil {
+			dot.Logger().Errorln("", zap.Error(err))
+			return
+		}
+		defer line.StopAndDestroy(l, true) //fourth step stop and destroy dots
 
-	ssignal.WaitCtrlC(func(s os.Signal) bool { //third wait for exit
-		return false
-	})
+		dot.Logger().Infoln("dot ok")
+		//second step ....
 
+		ssignal.WaitCtrlC(func(s os.Signal) bool { //third wait for exit
+			return false
+		})
+	}
 }
 
 func add(l dot.Line) error {
 	lives := nobl.HiServerTypeLives()
 	lives = append(lives, gserver.HttpNoblTypeLives()...)
 	return l.PreAdd(lives...)
+}
+func linuxBrowser(ch chan byte) {
+	go func() {
+		ch <- 1
+		err := exec.Command("x-www-browser", "http://localhost:9090").Run()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "please click ->   http://localhost:9090\n")
+		}
+	}()
+}
+func windowsBrowser(ch chan byte) {
+	go func() {
+		ch <- 1
+		err := exec.Command("cmd", "/C", "start", "http://localhost:9090").Run()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "please click ->   http://localhost:9090\n")
+		}
+	}()
 }
