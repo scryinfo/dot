31ff3ef3532e45b7d395450c533c550c14c7ee7f
babybabyboy <15884273030@163.com>
2019-08-20 09:26:17 +0800

    input number bug input bool bug fixed

--- tools/config/ui/apps/src/components/schemaGenerator/index.js
+++ tools/config/ui/apps/src/components/schemaGenerator/index.js
@@ -8,7 +8,9 @@ var jsonToSchema = function(json) {
   var compiler = new Compiler();
   var ast = new AST();
   ast.build(json);
+  console.log(JSON.stringify(ast.tree, null, 4));
   compiler.compile(ast.tree);
+  console.log(JSON.stringify(compiler.schema, null, 4));
   return compiler.schema;
 };
 
--- tools/config/ui/apps/src/components/schemaGenerator/utils.js
+++ tools/config/ui/apps/src/components/schemaGenerator/utils.js
@@ -24,61 +24,6 @@ let isBoolean = function(value) {
   return (value === true || value === false);
 };
 
-let toObject = function(arr) {
-  var rv = {};
-  for (var i = 0; i < arr.length; ++i)
-    rv[i] = arr[i];
-  return rv;
-};
-
-let oneIsNull = function(v1, v2) {
-  return ((v1 === null && v2 !== null) || (v1 !== null && v2 === null));
-};
-
-let isUndefined = function(val) {
-  return (null === val || typeof val === 'undefined');
-};
-
-let isFunction = function(fn) {
-  return (typeof fn === 'function');
-};
-
-let isEqual = function(v1, v2) {
-  if (typeof v1 !== typeof v2 || oneIsNull(v1, v2)) {
-    return false;
-  }
-
-  if (typeof v1 === typeof "" || typeof v1 === typeof 0) {
-    return v1 === v2;
-  }
-
-  var _isEqual = true;
-
-  if (typeof v1 === typeof {}) {
-    var compare = function(value1, value2) {
-      for (var i in value1) {
-        if (!value2.hasOwnProperty(i)) {
-          _isEqual = false;
-          break;
-        }
-
-        if (exports.isObject(value1[i])) {
-          compare(value1[i], value2[i]);
-        } else if (typeof value1[i] === typeof "") {
-          if (value1[i] !== value2[i]) {
-            _isEqual = false;
-            break;
-          }
-        }
-      }
-    }
-
-    compare(v1, v2);
-  }
-
-  return _isEqual;
-};
-
 let getType = function(data) {
   if (isObject(data)) {
     return 'object';
@@ -95,4 +40,4 @@ let getType = function(data) {
   }
 };
 
-export {isObject,isNumber,isArray,isString,isNull,isBoolean,toObject,oneIsNull,isUndefined,isFunction,isEqual,getType }
+export {isObject,isArray,getType }
