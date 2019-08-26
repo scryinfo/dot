4824b383990deefa990330857f45f2ba15cbc299
Peng-Kun-Dai <15202844993@163.com>
2019-08-26 18:09:31 +0800

    review code

--- tools/config/data/server/server.go
+++ tools/config/data/server/server.go
@@ -49,7 +49,7 @@ func main() {
 }
 
 func add(l dot.Line) error {
-	lives := nobl.HiServerTypeLives()
+	lives := nobl.RpcImplementTypeLives()
 	lives = append(lives, gserver.GinNoblTypeLives()...)
 	l.ToDotEventer().AddLiveEvents(dot.LiveId(gindot.EngineLiveId), &dot.LiveEvents{
 		AfterCreate: func(live *dot.Live, l dot.Line) {
