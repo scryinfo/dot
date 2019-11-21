451a3c6cc7134e5f3b81fd4fc2779caaf27ad3fc
Peacess <peacess@163.com>
2019-11-21 10:49:11 +0800

    update grpc gin

--- dots-update.bat
+++ dots-update.bat
@@ -7,9 +7,9 @@ set batPath=%cd%
 cd %batPath%/dots
 set dotPath=%cd%
 
-cd %dotPath%/gindot  & go get -u github.com/scryinfo/dot@master & go mod tidy
+cd %dotPath%/gindot  & go get -u github.com/scryinfo/dot & go mod tidy
 cd %dotPath%/grpc & go get -u github.com/scryinfo/dot/dots/gindot@master & go mod tidy
-cd %dotPath%/db/pgs & go get -u github.com/scryinfo/dot/@master & go mod tidy
+cd %dotPath%/db/pgs & go get -u github.com/scryinfo/dot & go mod tidy
 
 cd %batPath%
 
