3d94377664118f1e9952b5af0b19d3dfbd179caf
Peacess <peacess@163.com>
2019-12-05 19:29:28 +0800

    update pg to v9

--- dots/db/pgs/kits.go
+++ dots/db/pgs/kits.go
@@ -3,6 +3,9 @@ package pgs
 import (
 	"github.com/go-pg/pg/v9"
 	"github.com/go-pg/pg/v9/orm"
+	"reflect"
+	"strconv"
+	"strings"
 )
 
 func CreateSchema(db *pg.DB, ms []interface{}) error {
@@ -37,3 +40,138 @@ func SqlLikeEscape(param string, special []rune, escape rune) string {
 	res := string(newcs)
 	return res
 }
+
+//把结构体的字段转换为 map,  key为字段名首字母小写
+func ToMap(d interface{}, ex map[string]bool) map[string]string {
+	if ex == nil {
+		ex = map[string]bool{}
+	}
+	res := make(map[string]string)
+	mv := reflect.ValueOf(d).Elem()
+	mt := mv.Type()
+	for i := 0; i < mv.NumField(); i++ {
+		v := mv.Field(i)
+		fname := mt.Field(i).Name
+		lfname := ToFirstLower(fname)
+		if _, ok := ex[fname]; ok {
+			continue
+		}
+
+		if _, ok := ex[lfname]; ok {
+			continue
+		}
+		switch v.Kind() {
+		case reflect.Int64, reflect.Int32, reflect.Int, reflect.Int16:
+			res[lfname] = strconv.FormatInt(v.Int(), 10)
+		default:
+			res[lfname] = v.String()
+		}
+	}
+
+	return res
+}
+
+//首字母小写
+func ToFirstLower(s string) string {
+	cs := []rune(s)
+	return strings.ToLower(string(cs[0])) + string(cs[1:])
+}
+
+/////////////////////////////////////////////see github.com/go-pg/pg/internal
+func isUpper(c byte) bool {
+	return c >= 'A' && c <= 'Z'
+}
+
+func isLower(c byte) bool {
+	return c >= 'a' && c <= 'z'
+}
+
+func toUpper(c byte) byte {
+	return c - 32
+}
+
+func toLower(c byte) byte {
+	return c + 32
+}
+
+// Underscore converts "CamelCasedString" to "camel_cased_string".
+// Just work for ASCⅡ code
+func Underscore(s string) string {
+	r := make([]byte, 0, len(s)+5)
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if isUpper(c) {
+			if i > 0 && i+1 < len(s) && (isLower(s[i-1]) || isLower(s[i+1])) {
+				r = append(r, '_', toLower(c))
+			} else {
+				r = append(r, toLower(c))
+			}
+		} else {
+			r = append(r, c)
+		}
+	}
+	return string(r)
+}
+
+// Just work for ASCⅡ code
+func CamelCased(s string) string {
+	r := make([]byte, 0, len(s))
+	upperNext := true
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if c == '_' {
+			upperNext = true
+			continue
+		}
+		if upperNext {
+			if isLower(c) {
+				c = toUpper(c)
+			}
+			upperNext = false
+		}
+		r = append(r, c)
+	}
+	return string(r)
+}
+
+// Just work for ASCⅡ code
+func ToExported(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+	if c := s[0]; isLower(c) {
+		b := []byte(s)
+		b[0] = toUpper(c)
+		return string(b)
+	}
+	return s
+}
+
+// Just work for ASCⅡ code
+func UpperString(s string) string {
+	if isUpperString(s) {
+		return s
+	}
+
+	b := make([]byte, len(s))
+	for i := range b {
+		c := s[i]
+		if isLower(c) {
+			c = toUpper(c)
+		}
+		b[i] = c
+	}
+	return string(b)
+}
+
+func isUpperString(s string) bool {
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if isLower(c) {
+			return false
+		}
+	}
+	return true
+}
+
+/////////////////////////////////////////////see github.com/go-pg/pg/internal
--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.12
 require (
 	github.com/bitly/go-simplejson v0.5.0
 	github.com/pkg/errors v0.8.1
-	github.com/scryinfo/dot/dots/grpc v0.0.0-20191121024911-104dd77f0dab // indirect
 	github.com/scryinfo/scryg v0.1.3-0.20190608053141-a292b801bfd6
 	go.uber.org/zap v1.13.0
 )
