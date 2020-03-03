6601e58fea664dd15fc5968655be239e4af55bac
Zealy <ljg_cqu@126.com>
2020-03-03 17:00:20 +0800

    adjust the implementation idea, and refactor the code.

--- dots/grpc/gserver/websocket.go
+++ dots/grpc/gserver/websocket.go
@@ -6,8 +6,10 @@ package gserver
 import (
 	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"github.com/improbable-eng/grpc-web/go/grpcweb"
+	"google.golang.org/grpc"
+
+	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 
 	"github.com/scryinfo/dot/dot"
@@ -18,41 +20,31 @@ const (
 	WebSocketTypeId = "27709862-0a7b-48e9-8427-5bbc86760c41"
 )
 
-// WebSocket component makes it possible the communication between a gRPC-WebSocket request and a standard gRPC server.
+// WebSocket component makes it possible the full-duplex communication with low latency
+// between a gRPC-WebSocket request and a remote standard gRPC server.
 type WebSocket struct {
-	ServerNobl ServerNobl     `dot:""`
-	GinRouter  *gindot.Router `dot:""`
+	GinEngine *gindot.Engine `dot:""`
 
 	wrappedServer *grpcweb.WrappedGrpcServer
 }
 
-func WebSocketTypeLive() *dot.TypeLives {
-	newWebSocket := func(conf []byte) (dot.Dot, error) {
-		return &WebSocket{}, nil
-	}
-
-	return &dot.TypeLives{
-		Meta: dot.Metadata{TypeId: WebSocketTypeId, NewDoter: newWebSocket},
-		Lives: []dot.Live{
-			{
-				TypeId: WebSocketTypeId,
-				RelyLives: map[string]dot.LiveId{
-					"ServerNobl": ServerNoblTypeId,
-					"GinRouter":  gindot.RouterTypeId,
+func WebSocketTypeLives() []*dot.TypeLives {
+	websocktTypeLive := func() *dot.TypeLives {
+		return &dot.TypeLives{
+			Meta: dot.Metadata{TypeId: WebSocketTypeId, NewDoter: func(conf []byte) (dot.Dot, error) {
+				return &WebSocket{}, nil
+			}},
+			Lives: []dot.Live{
+				{
+					TypeId: WebSocketTypeId,
+					RelyLives: map[string]dot.LiveId{
+						"GinEngine": gindot.EngineTypeId,
+					},
 				},
 			},
-		},
+		}
 	}
-}
-
-func WebSocketAndRelyTypeLives() []*dot.TypeLives {
-	typeLives := []*dot.TypeLives{WebSocketTypeLive(), ServerNoblTypeLive()}
-	typeLives = append(typeLives, gindot.TypeLiveRouter()...)
-	return typeLives
-}
-
-func (s *WebSocket) AfterAllStart(l dot.Line) {
-	s.serve()
+	return []*dot.TypeLives{websocktTypeLive(), gindot.TypeLiveGinDot()}
 }
 
 func (s *WebSocket) Stop(ignore bool) error {
@@ -62,7 +54,19 @@ func (s *WebSocket) Stop(ignore bool) error {
 	return nil
 }
 
-func (s *WebSocket) serve() {
+// GET wraps the given grpcServer to allow for handling grpc-web requests of websockets - enabling bidirectional requests.
+//
+// Under the hood, GET takes a HTTP request from gin.Context and if it is a gRPC-WebSocket request wraps it with a compatibility layer
+// to transform it to a standard gRPC request for the wrapped gRPC server and transforms the request to comply with
+// the gRPC-Web protocol.
+//
+// Through this mechanism, client (e.g. browser) is able to fully take advantage of WebSocket communication with remote
+// gRPC service server, initially routed by the HTTP GET method and the given path.
+//
+// Note: this GET method can only be called before the underlying gin.Engine starts running, besides the caller must insure that
+// all the grpc service servers get appropriately registered with the standard gRPC server.
+func (s *WebSocket) GET(relativePath string, grpcServer *grpc.Server) {
+	// wrap grpcServer to allow for handling grpc-web requests of websockets - enabling bidirectional requests.
 	options := []grpcweb.Option{
 		grpcweb.WithWebsockets(true),
 		grpcweb.WithWebsocketOriginFunc(func(req *http.Request) bool {
@@ -73,8 +77,11 @@ func (s *WebSocket) serve() {
 		}),
 		grpcweb.WithCorsForRegisteredEndpointsOnly(false),
 	}
-	s.wrappedServer = grpcweb.WrapServer(s.ServerNobl.Server(), options...)
+	s.wrappedServer = grpcweb.WrapServer(grpcServer, options...)
 
+	// handlerFunc takes a HTTP request from gin.Context and if it is a gRPC-WebSocket request wraps it with a compatibility layer
+	// to transform it to a standard gRPC request for the wrapped gRPC server and transforms the request to comply with
+	// the gRPC-Web protocol.
 	handlerFunc := func(ctx *gin.Context) {
 		dot.Logger().Debugln("WebSocket", zap.String("", ctx.Request.RequestURI))
 
@@ -86,5 +93,6 @@ func (s *WebSocket) serve() {
 		ctx.String(http.StatusForbidden, "Not valid gRPC-WebSocket request!")
 	}
 
-	s.GinRouter.Router().GET("/*grpc-websocket", handlerFunc)
+	// registers the new request handle (without any middleware) with the given path and GET method with gin.Engine.
+	s.GinEngine.GinEngine().GET(relativePath, handlerFunc)
 }
