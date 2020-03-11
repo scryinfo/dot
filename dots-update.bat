3faf59f2636940e155e5b62b98ac7f7a740620ac
Peacess <peacess@163.com>
2020-03-11 11:13:29 +0800

    update mod

--- dots-update.bat
+++ dots-update.bat
@@ -7,9 +7,9 @@ set batPath=%cd%
 cd %batPath%/dots
 set dotPath=%cd%
 
-cd %dotPath%/gindot  & go get -u github.com/scryinfo/dot & go mod tidy
-cd %dotPath%/grpc & go get -u github.com/scryinfo/dot/dots/gindot@master & go mod tidy
-cd %dotPath%/db/pgs & go get -u github.com/scryinfo/dot & go mod tidy
+cd %dotPath%/gindot  & go get github.com/scryinfo/dot
+cd %dotPath%/grpc & go get github.com/scryinfo/dot/dots/gindot@master
+cd %dotPath%/db/pgs & go get github.com/scryinfo/dot
 
 cd %batPath%
 
