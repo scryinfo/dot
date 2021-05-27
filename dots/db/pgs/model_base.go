51cda4ed411413efb39ce1bfae05660a3583c940
mats9693 <982254249@qq.com>
2021-05-27 10:54:48 +0800

    use standard writing

--- dots/db/pgs/model_base.go
+++ dots/db/pgs/model_base.go
@@ -1,7 +1,7 @@
 package pgs
 
 type ModelBase struct {
-	ID                    string `pk  json:"id"`                               //id
+	ID                    string `pg:",pk" json:"id"`                          //id
 	UpdateTime            int64  `json:"updateTime"`                           //更新时间
 	CreateTime            int64  `json:"createTime"`                           //创建时间
 	OptimisticLockVersion int64  `pg:",use_zero" json:"optimisticLockVersion"` //default 0 ，not null
