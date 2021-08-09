a022fab598c60430efa8e8e5d3fa2bd7c03ec265
jayce <15202844993@163.com>
2021-08-09 19:43:45 +0800

    temporarily annotate the etcd component

--- dots/grpc/conns/etcd_conns.go
+++ dots/grpc/conns/etcd_conns.go
@@ -1,217 +1,218 @@
 package conns
 
-import (
-	"context"
-	"crypto/tls"
-	"encoding/json"
-	"time"
-
-	"github.com/pkg/errors"
-	"github.com/scryinfo/dot/dot"
-	"github.com/scryinfo/dot/utils"
-	clientv3 "go.etcd.io/etcd/client/v3"
-	"go.etcd.io/etcd/client/v3/naming/endpoints"
-	"go.etcd.io/etcd/client/v3/naming/resolver"
-	"google.golang.org/grpc"
-	gresolver "google.golang.org/grpc/resolver"
-)
-
-const EtcdConnsTypeID = "c91c8c68-281a-4949-8e55-3516664e80c7"
-
-//see https://github.com/etcd-io/etcd/blob/master/Documentation/dev-guide/grpc_naming.md
-
-type configEtcdConns struct {
-	Endpoints   []string        `json:"endpoints"`
-	DialTimeout int64           `json:"dialTimeout"`
-	Tls         utils.TlsConfig `json:"tls"` //not sure about the Mutual Authentication can work
-
-	Names []string `json:"names"` //服务名字
-}
-
-//etcd client
-type EtcdConns struct {
-	etcdClient   *clientv3.Client
-	grpcManager  endpoints.Manager //ref the etcdClient
-	grpcResolver gresolver.Builder
-	conns        map[string]*grpc.ClientConn
-	conf         configEtcdConns
-	ctx          context.Context
-	cancelFun    context.CancelFunc
-}
-
-func (c *EtcdConns) ClientConn(serviceName string) *grpc.ClientConn {
-	var conn *grpc.ClientConn = nil
-	if len(c.conns) > 0 {
-		if c, ok := c.conns[serviceName]; ok {
-			conn = c
-		}
-	}
-	return conn
-}
-
-func (c *EtcdConns) EtcdClient() *clientv3.Client {
-	return c.etcdClient
-}
-
-func (c *EtcdConns) Context() context.Context {
-	return c.ctx
-}
-
-func (c *EtcdConns) CancelFun() context.CancelFunc {
-	return c.cancelFun
-}
-
-func (c *EtcdConns) Manager() endpoints.Manager {
-	return c.grpcManager
-}
-func (c *EtcdConns) Builder() gresolver.Builder {
-	return c.grpcResolver
-}
-func (c *EtcdConns) RegisterServer(ctx context.Context, name string, addr string) error {
-	if c.grpcManager != nil {
-		opt := make([]*endpoints.UpdateWithOpts, 1)
-		opt[0] = endpoints.NewAddUpdateOpts(name, endpoints.Endpoint{
-			Addr:     addr,
-			Metadata: nil,
-		})
-		return c.grpcManager.Update(ctx, opt)
-	}
-	return errors.New("GRPC Resolver is null")
-}
-func (c *EtcdConns) UnRegisterServer(ctx context.Context, name string, addr string) error {
-	if c.grpcManager != nil {
-		opt := make([]*endpoints.UpdateWithOpts, 1)
-		opt[0] = endpoints.NewAddUpdateOpts(name, endpoints.Endpoint{
-			Addr:     addr,
-			Metadata: nil,
-		})
-		return c.grpcManager.DeleteEndpoint(ctx, name)
-	}
-	return errors.New("GRPC Resolver is null")
-}
-
-//func (c *EtcdConns) Create(l dot.Line) error {
 //
-//}
-//func (c *EtcdConns) Injected(l dot.Line) error {
+//import (
+//	"context"
+//	"crypto/tls"
+//	"encoding/json"
+//	"time"
 //
-//}
-//func (c *EtcdConns) AfterAllInject(l dot.Line) {
+//	"github.com/pkg/errors"
+//	"github.com/scryinfo/dot/dot"
+//	"github.com/scryinfo/dot/utils"
+//	clientv3 "go.etcd.io/etcd/client/v3"
+//	"go.etcd.io/etcd/client/v3/naming/endpoints"
+//	"go.etcd.io/etcd/client/v3/naming/resolver"
+//	"google.golang.org/grpc"
+//	gresolver "google.golang.org/grpc/resolver"
+//)
 //
+//const EtcdConnsTypeID = "c91c8c68-281a-4949-8e55-3516664e80c7"
+//
+////see https://github.com/etcd-io/etcd/blob/master/Documentation/dev-guide/grpc_naming.md
+//
+//type configEtcdConns struct {
+//	Endpoints   []string        `json:"endpoints"`
+//	DialTimeout int64           `json:"dialTimeout"`
+//	Tls         utils.TlsConfig `json:"tls"` //not sure about the Mutual Authentication can work
+//
+//	Names []string `json:"names"` //服务名字
 //}
 //
-//func (c *EtcdConns) Start(ignore bool) error {
-//
+////etcd client
+//type EtcdConns struct {
+//	etcdClient   *clientv3.Client
+//	grpcManager  endpoints.Manager //ref the etcdClient
+//	grpcResolver gresolver.Builder
+//	conns        map[string]*grpc.ClientConn
+//	conf         configEtcdConns
+//	ctx          context.Context
+//	cancelFun    context.CancelFunc
 //}
-
-func (c *EtcdConns) Stop(ignore bool) error {
-	if c.cancelFun != nil {
-		c.cancelFun()
-	}
-	if c.etcdClient != nil {
-		c.etcdClient.Close()
-		//c.etcdClient = nil // maybe somewhere use the client, so do not set nil
-	}
-	return nil
-}
-
-//func (c *EtcdConns) Destroy(ignore bool) error {
 //
+//func (c *EtcdConns) ClientConn(serviceName string) *grpc.ClientConn {
+//	var conn *grpc.ClientConn = nil
+//	if len(c.conns) > 0 {
+//		if c, ok := c.conns[serviceName]; ok {
+//			conn = c
+//		}
+//	}
+//	return conn
 //}
-
-//construct dot
-func newEtcdConns(conf []byte) (dot.Dot, error) {
-	dconf := &configEtcdConns{}
-	err := dot.UnMarshalConfig(conf, dconf)
-	if err != nil {
-		return nil, err
-	}
-
-	d := &EtcdConns{conf: *dconf, conns: make(map[string]*grpc.ClientConn)}
-	d.ctx, d.cancelFun = context.WithCancel(context.Background())
-	{
-		var tlsConfig *tls.Config
-		{
-			tlsConfig, err = utils.GetTlsConfig(&d.conf.Tls)
-			if err != nil {
-				return nil, err
-			}
-		}
-		if d.conf.DialTimeout < 1 {
-			d.conf.DialTimeout = 6
-		}
-
-		d.etcdClient, err = clientv3.New(clientv3.Config{
-			Endpoints:   d.conf.Endpoints,
-			DialTimeout: time.Duration(d.conf.DialTimeout) * time.Second,
-			TLS:         tlsConfig,
-			Context:     d.ctx,
-		})
-		if err != nil {
-			return nil, err
-		}
-	}
-	//todo target
-	d.grpcManager, err = endpoints.NewManager(d.etcdClient, "")
-	if err != nil {
-		return nil, err
-	}
-	d.grpcResolver, err = resolver.NewBuilder(d.etcdClient)
-	if err != nil {
-		return nil, err
-	}
-	if len(d.conf.Names) > 0 {
-
-		for _, n := range d.conf.Names {
-			conn, err := grpc.Dial(n, grpc.WithResolvers(d.grpcResolver), grpc.WithInsecure())
-			if err != nil {
-				return nil, err
-			}
-			d.conns[n] = conn
-		}
-	}
-
-	return d, nil
-}
-
-//EtcdConnsTypeLives
-func EtcdConnsTypeLives() []*dot.TypeLives {
-	tl := &dot.TypeLives{
-		Meta: dot.Metadata{TypeID: EtcdConnsTypeID, NewDoter: func(conf []byte) (dot.Dot, error) {
-			return newEtcdConns(conf)
-		}},
-		//Lives: []dot.Live{
-		//	{
-		//		LiveID:    EtcdConnsTypeID,
-		//		RelyLives: map[string]dot.LiveID{"some field": "some id"},
-		//	},
-		//},
-	}
-
-	lives := []*dot.TypeLives{tl}
-
-	return lives
-}
-
-//EtcdConnsConfigTypeLive
-func EtcdConnsConfigTypeLive() *dot.ConfigTypeLive {
-	paths := make([]string, 0)
-	paths = append(paths, "")
-	return &dot.ConfigTypeLive{
-		TypeIDConfig: EtcdConnsTypeID,
-		ConfigInfo:   &configEtcdConns{},
-	}
-}
-
-func NewEtcd(endpoints []string, names []string) *EtcdConns {
-	conf := &configEtcdConns{
-		Endpoints:   endpoints,
-		DialTimeout: 10,
-		Tls:         utils.TlsConfig{},
-		Names:       names,
-	}
-	bs, _ := json.Marshal(conf)
-	re, _ := newEtcdConns(bs)
-	etcdConns, _ := re.(*EtcdConns)
-	return etcdConns
-}
+//
+//func (c *EtcdConns) EtcdClient() *clientv3.Client {
+//	return c.etcdClient
+//}
+//
+//func (c *EtcdConns) Context() context.Context {
+//	return c.ctx
+//}
+//
+//func (c *EtcdConns) CancelFun() context.CancelFunc {
+//	return c.cancelFun
+//}
+//
+//func (c *EtcdConns) Manager() endpoints.Manager {
+//	return c.grpcManager
+//}
+//func (c *EtcdConns) Builder() gresolver.Builder {
+//	return c.grpcResolver
+//}
+//func (c *EtcdConns) RegisterServer(ctx context.Context, name string, addr string) error {
+//	if c.grpcManager != nil {
+//		opt := make([]*endpoints.UpdateWithOpts, 1)
+//		opt[0] = endpoints.NewAddUpdateOpts(name, endpoints.Endpoint{
+//			Addr:     addr,
+//			Metadata: nil,
+//		})
+//		return c.grpcManager.Update(ctx, opt)
+//	}
+//	return errors.New("GRPC Resolver is null")
+//}
+//func (c *EtcdConns) UnRegisterServer(ctx context.Context, name string, addr string) error {
+//	if c.grpcManager != nil {
+//		opt := make([]*endpoints.UpdateWithOpts, 1)
+//		opt[0] = endpoints.NewAddUpdateOpts(name, endpoints.Endpoint{
+//			Addr:     addr,
+//			Metadata: nil,
+//		})
+//		return c.grpcManager.DeleteEndpoint(ctx, name)
+//	}
+//	return errors.New("GRPC Resolver is null")
+//}
+//
+////func (c *EtcdConns) Create(l dot.Line) error {
+////
+////}
+////func (c *EtcdConns) Injected(l dot.Line) error {
+////
+////}
+////func (c *EtcdConns) AfterAllInject(l dot.Line) {
+////
+////}
+////
+////func (c *EtcdConns) Start(ignore bool) error {
+////
+////}
+//
+//func (c *EtcdConns) Stop(ignore bool) error {
+//	if c.cancelFun != nil {
+//		c.cancelFun()
+//	}
+//	if c.etcdClient != nil {
+//		c.etcdClient.Close()
+//		//c.etcdClient = nil // maybe somewhere use the client, so do not set nil
+//	}
+//	return nil
+//}
+//
+////func (c *EtcdConns) Destroy(ignore bool) error {
+////
+////}
+//
+////construct dot
+//func newEtcdConns(conf []byte) (dot.Dot, error) {
+//	dconf := &configEtcdConns{}
+//	err := dot.UnMarshalConfig(conf, dconf)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	d := &EtcdConns{conf: *dconf, conns: make(map[string]*grpc.ClientConn)}
+//	d.ctx, d.cancelFun = context.WithCancel(context.Background())
+//	{
+//		var tlsConfig *tls.Config
+//		{
+//			tlsConfig, err = utils.GetTlsConfig(&d.conf.Tls)
+//			if err != nil {
+//				return nil, err
+//			}
+//		}
+//		if d.conf.DialTimeout < 1 {
+//			d.conf.DialTimeout = 6
+//		}
+//
+//		d.etcdClient, err = clientv3.New(clientv3.Config{
+//			Endpoints:   d.conf.Endpoints,
+//			DialTimeout: time.Duration(d.conf.DialTimeout) * time.Second,
+//			TLS:         tlsConfig,
+//			Context:     d.ctx,
+//		})
+//		if err != nil {
+//			return nil, err
+//		}
+//	}
+//	//todo target
+//	d.grpcManager, err = endpoints.NewManager(d.etcdClient, "")
+//	if err != nil {
+//		return nil, err
+//	}
+//	d.grpcResolver, err = resolver.NewBuilder(d.etcdClient)
+//	if err != nil {
+//		return nil, err
+//	}
+//	if len(d.conf.Names) > 0 {
+//
+//		for _, n := range d.conf.Names {
+//			conn, err := grpc.Dial(n, grpc.WithResolvers(d.grpcResolver), grpc.WithInsecure())
+//			if err != nil {
+//				return nil, err
+//			}
+//			d.conns[n] = conn
+//		}
+//	}
+//
+//	return d, nil
+//}
+//
+////EtcdConnsTypeLives
+//func EtcdConnsTypeLives() []*dot.TypeLives {
+//	tl := &dot.TypeLives{
+//		Meta: dot.Metadata{TypeID: EtcdConnsTypeID, NewDoter: func(conf []byte) (dot.Dot, error) {
+//			return newEtcdConns(conf)
+//		}},
+//		//Lives: []dot.Live{
+//		//	{
+//		//		LiveID:    EtcdConnsTypeID,
+//		//		RelyLives: map[string]dot.LiveID{"some field": "some id"},
+//		//	},
+//		//},
+//	}
+//
+//	lives := []*dot.TypeLives{tl}
+//
+//	return lives
+//}
+//
+////EtcdConnsConfigTypeLive
+//func EtcdConnsConfigTypeLive() *dot.ConfigTypeLive {
+//	paths := make([]string, 0)
+//	paths = append(paths, "")
+//	return &dot.ConfigTypeLive{
+//		TypeIDConfig: EtcdConnsTypeID,
+//		ConfigInfo:   &configEtcdConns{},
+//	}
+//}
+//
+//func NewEtcd(endpoints []string, names []string) *EtcdConns {
+//	conf := &configEtcdConns{
+//		Endpoints:   endpoints,
+//		DialTimeout: 10,
+//		Tls:         utils.TlsConfig{},
+//		Names:       names,
+//	}
+//	bs, _ := json.Marshal(conf)
+//	re, _ := newEtcdConns(bs)
+//	etcdConns, _ := re.(*EtcdConns)
+//	return etcdConns
+//}
--- dots/grpc/gserver/etcd_register.go
+++ dots/grpc/gserver/etcd_register.go
@@ -1,175 +1,176 @@
 package gserver
 
-import (
-	"github.com/scryinfo/dot/dot"
-	"github.com/scryinfo/dot/dots/grpc/conns"
-	"go.uber.org/zap"
-	"runtime"
-)
-
-const EtcdRegisterTypeID = "c2c90392-ed06-4e7f-ac2a-662b0153ecb5"
-
-type ServerItem struct {
-	Name  string   `json:"name"`
-	Addrs []string `json:"addrs"` //可以对应多个地址
-}
-
-//do not config, if the ServerNobl in the current process
-type ConfigEtcdRegister struct {
-	Items []ServerItem
-}
-type EtcdRegister struct {
-	EtcdConns *conns.EtcdConns `dot:""`
-	items     []ServerItem
-	conf      ConfigEtcdRegister
-}
-
-//func (c *EtcdRegister) Create(l dot.Line) error {
-//	//todo add
-//}
-//func (c *EtcdRegister) Injected(l dot.Line) error {
-//	//todo add
-//}
-func (c *EtcdRegister) AfterAllInject(l dot.Line) {
-	if l != nil {
-		var items []ServerItem
-		l.EachLives(func(live *dot.Live, metadata *dot.Metadata) bool {
-			if live == nil {
-				return true
-			}
-			if s, ok := live.Dot.(ServerNobl); ok {
-				items = append(items, s.ServerItem())
-			}
-			return true
-		})
-		nameItems := make(map[string]*ServerItem, len(items)+len(c.conf.Items))
-		putToMap := func(nameMap map[string]*ServerItem, serverItems []ServerItem) {
-			for i := range serverItems {
-				item := &serverItems[i]
-				mapItem := nameMap[item.Name]
-				if mapItem == nil {
-					mapItem = &ServerItem{Name: item.Name}
-					nameMap[item.Name] = mapItem
-				}
-				for _, addr := range item.Addrs {
-					exist := false
-					for _, old := range mapItem.Addrs { //find the new addr
-						if addr == old {
-							exist = true
-							break
-						}
-					}
-					if !exist { //if it new, append it
-						mapItem.Addrs = append(mapItem.Addrs, addr)
-					}
-				}
-			}
-		}
-		putToMap(nameItems, c.conf.Items)
-		putToMap(nameItems, items)
-
-		c.items = make([]ServerItem, 0, len(nameItems))
-		for _, item := range nameItems {
-			c.items = append(c.items, *item)
-		}
-	}
-}
-
 //
-//func (c *EtcdRegister) Start(ignore bool) error {
+//import (
+//	"github.com/scryinfo/dot/dot"
+//	"github.com/scryinfo/dot/dots/grpc/conns"
+//	"go.uber.org/zap"
+//	"runtime"
+//)
 //
-//}
-
-func (c *EtcdRegister) AfterAllStart() {
-	logger := dot.Logger()
-	if c.EtcdConns != nil {
-		go func() {
-			for i := range c.items {
-				item := &c.items[i]
-				for _, addr := range item.Addrs {
-					err := c.EtcdConns.RegisterServer(c.EtcdConns.Context(), item.Name, addr)
-					if err != nil {
-						logger.Infoln("EtcdRegister", zap.Error(err))
-					}
-				}
-			}
-		}()
-
-		runtime.Gosched() //给goroutine运行机会
-	}
-}
-
-func (c *EtcdRegister) Stop(ignore bool) error {
-	logger := dot.Logger()
-	var err error
-	if c.EtcdConns != nil {
-		for i := range c.items {
-			item := &c.items[i]
-			for _, addr := range item.Addrs {
-				err2 := c.EtcdConns.UnRegisterServer(c.EtcdConns.Context(), item.Name, addr)
-				if err2 != nil {
-					err = err2
-					logger.Infoln("EtcdRegister", zap.Error(err))
-				}
-			}
-		}
-	}
-	return err
-}
-
-//func (c *EtcdRegister) Destroy(ignore bool) error {
+//const EtcdRegisterTypeID = "c2c90392-ed06-4e7f-ac2a-662b0153ecb5"
 //
+//type ServerItem struct {
+//	Name  string   `json:"name"`
+//	Addrs []string `json:"addrs"` //可以对应多个地址
 //}
-
-//construct dot
-func newEtcdRegister(conf []byte) (dot.Dot, error) {
-	dconf := &ConfigEtcdRegister{}
-	_ = dot.UnMarshalConfig(conf, dconf)
-	//if err != nil {
-	//	return nil, err
-	//}
-
-	d := &EtcdRegister{conf: *dconf}
-	return d, nil
-}
-
-//NewEctcRegisterTest for test
-func NewEctcRegisterTest(config *ConfigEtcdRegister) *EtcdRegister {
-	if config == nil {
-		config = &ConfigEtcdRegister{}
-	}
-	d := &EtcdRegister{conf: *config}
-	return d
-}
-
-//EtcdRegisterTypeLives
-func EtcdRegisterTypeLives() []*dot.TypeLives {
-	tl := &dot.TypeLives{
-		Meta: dot.Metadata{TypeID: EtcdRegisterTypeID, NewDoter: func(conf []byte) (dot.Dot, error) {
-			return newEtcdRegister(conf)
-		}},
-		Lives: []dot.Live{
-			{
-				LiveID:    EtcdRegisterTypeID,
-				RelyLives: map[string]dot.LiveID{"EtcdConns": conns.EtcdConnsTypeID},
-			},
-		},
-	}
-
-	lives := []*dot.TypeLives{tl}
-	lives = append(lives, conns.EtcdConnsTypeLives()...)
-
-	return lives
-}
-
-//EtcdRegisterConfigTypeLive
-func EtcdRegisterConfigTypeLive() *dot.ConfigTypeLive {
-	paths := make([]string, 0)
-	paths = append(paths, "")
-	return &dot.ConfigTypeLive{
-		TypeIDConfig: EtcdRegisterTypeID,
-		ConfigInfo: &ConfigEtcdRegister{
-			Items: []ServerItem{},
-		},
-	}
-}
+//
+////do not config, if the ServerNobl in the current process
+//type ConfigEtcdRegister struct {
+//	Items []ServerItem
+//}
+//type EtcdRegister struct {
+//	EtcdConns *conns.EtcdConns `dot:""`
+//	items     []ServerItem
+//	conf      ConfigEtcdRegister
+//}
+//
+////func (c *EtcdRegister) Create(l dot.Line) error {
+////	//todo add
+////}
+////func (c *EtcdRegister) Injected(l dot.Line) error {
+////	//todo add
+////}
+//func (c *EtcdRegister) AfterAllInject(l dot.Line) {
+//	if l != nil {
+//		var items []ServerItem
+//		l.EachLives(func(live *dot.Live, metadata *dot.Metadata) bool {
+//			if live == nil {
+//				return true
+//			}
+//			if s, ok := live.Dot.(ServerNobl); ok {
+//				items = append(items, s.ServerItem())
+//			}
+//			return true
+//		})
+//		nameItems := make(map[string]*ServerItem, len(items)+len(c.conf.Items))
+//		putToMap := func(nameMap map[string]*ServerItem, serverItems []ServerItem) {
+//			for i := range serverItems {
+//				item := &serverItems[i]
+//				mapItem := nameMap[item.Name]
+//				if mapItem == nil {
+//					mapItem = &ServerItem{Name: item.Name}
+//					nameMap[item.Name] = mapItem
+//				}
+//				for _, addr := range item.Addrs {
+//					exist := false
+//					for _, old := range mapItem.Addrs { //find the new addr
+//						if addr == old {
+//							exist = true
+//							break
+//						}
+//					}
+//					if !exist { //if it new, append it
+//						mapItem.Addrs = append(mapItem.Addrs, addr)
+//					}
+//				}
+//			}
+//		}
+//		putToMap(nameItems, c.conf.Items)
+//		putToMap(nameItems, items)
+//
+//		c.items = make([]ServerItem, 0, len(nameItems))
+//		for _, item := range nameItems {
+//			c.items = append(c.items, *item)
+//		}
+//	}
+//}
+//
+////
+////func (c *EtcdRegister) Start(ignore bool) error {
+////
+////}
+//
+//func (c *EtcdRegister) AfterAllStart() {
+//	logger := dot.Logger()
+//	if c.EtcdConns != nil {
+//		go func() {
+//			for i := range c.items {
+//				item := &c.items[i]
+//				for _, addr := range item.Addrs {
+//					err := c.EtcdConns.RegisterServer(c.EtcdConns.Context(), item.Name, addr)
+//					if err != nil {
+//						logger.Infoln("EtcdRegister", zap.Error(err))
+//					}
+//				}
+//			}
+//		}()
+//
+//		runtime.Gosched() //给goroutine运行机会
+//	}
+//}
+//
+//func (c *EtcdRegister) Stop(ignore bool) error {
+//	logger := dot.Logger()
+//	var err error
+//	if c.EtcdConns != nil {
+//		for i := range c.items {
+//			item := &c.items[i]
+//			for _, addr := range item.Addrs {
+//				err2 := c.EtcdConns.UnRegisterServer(c.EtcdConns.Context(), item.Name, addr)
+//				if err2 != nil {
+//					err = err2
+//					logger.Infoln("EtcdRegister", zap.Error(err))
+//				}
+//			}
+//		}
+//	}
+//	return err
+//}
+//
+////func (c *EtcdRegister) Destroy(ignore bool) error {
+////
+////}
+//
+////construct dot
+//func newEtcdRegister(conf []byte) (dot.Dot, error) {
+//	dconf := &ConfigEtcdRegister{}
+//	_ = dot.UnMarshalConfig(conf, dconf)
+//	//if err != nil {
+//	//	return nil, err
+//	//}
+//
+//	d := &EtcdRegister{conf: *dconf}
+//	return d, nil
+//}
+//
+////NewEctcRegisterTest for test
+//func NewEctcRegisterTest(config *ConfigEtcdRegister) *EtcdRegister {
+//	if config == nil {
+//		config = &ConfigEtcdRegister{}
+//	}
+//	d := &EtcdRegister{conf: *config}
+//	return d
+//}
+//
+////EtcdRegisterTypeLives
+//func EtcdRegisterTypeLives() []*dot.TypeLives {
+//	tl := &dot.TypeLives{
+//		Meta: dot.Metadata{TypeID: EtcdRegisterTypeID, NewDoter: func(conf []byte) (dot.Dot, error) {
+//			return newEtcdRegister(conf)
+//		}},
+//		Lives: []dot.Live{
+//			{
+//				LiveID:    EtcdRegisterTypeID,
+//				RelyLives: map[string]dot.LiveID{"EtcdConns": conns.EtcdConnsTypeID},
+//			},
+//		},
+//	}
+//
+//	lives := []*dot.TypeLives{tl}
+//	lives = append(lives, conns.EtcdConnsTypeLives()...)
+//
+//	return lives
+//}
+//
+////EtcdRegisterConfigTypeLive
+//func EtcdRegisterConfigTypeLive() *dot.ConfigTypeLive {
+//	paths := make([]string, 0)
+//	paths = append(paths, "")
+//	return &dot.ConfigTypeLive{
+//		TypeIDConfig: EtcdRegisterTypeID,
+//		ConfigInfo: &ConfigEtcdRegister{
+//			Items: []ServerItem{},
+//		},
+//	}
+//}
