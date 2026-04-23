de5405858debf0f3d843537e06167bbfec793940
Peace <peacess@163.com>
2026-04-23 17:13:22 +0800

    run connect-web

--- samples/rpc/http/client/src/App.vue
+++ samples/rpc/http/client/src/App.vue
@@ -1,11 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Home from '@/views/Home.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Home></Home>
 </template>
 
 <style scoped></style>
